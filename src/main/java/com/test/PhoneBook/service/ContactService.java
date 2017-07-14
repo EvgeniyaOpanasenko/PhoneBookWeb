7006821fee4a0c93eef4180c1bf2bb0cffc7e6fd
evgeniya.opanasenko@outlook.com <63860DbnZ!>
2017-07-14 19:37:05 +0300

    secured annotation add

--- src/main/java/com/test/PhoneBook/service/ContactService.java
+++ src/main/java/com/test/PhoneBook/service/ContactService.java
@@ -2,13 +2,18 @@ package com.test.PhoneBook.service;
 
 
 import com.test.PhoneBook.model.Contact;
+import org.springframework.security.access.annotation.Secured;
 
 import java.util.List;
 
 public interface ContactService {
 
+    @Secured({"ROLE_ADMIN"})
     List<Contact> getAllContacts();
+
     boolean addContact(Contact contact);
+
     boolean deleteContact(Contact contact);
+
     boolean editContact(Contact contact);
 }
--- src/main/java/com/test/PhoneBook/service/UserService.java
+++ src/main/java/com/test/PhoneBook/service/UserService.java
@@ -1,11 +1,9 @@
 package com.test.PhoneBook.service;
 
 import com.test.PhoneBook.model.Contact;
-import org.springframework.security.access.annotation.Secured;
 
 import java.util.List;
 
 public interface UserService {
-	 @Secured({"ROLE_ADMIN"})
-     List<Contact> getAllUserContacts();
+    List<Contact> getAllUserContacts();
 }
