54367c51c5b1c1247554a2929f5a41786798d924
evgeniya.opanasenko@outlook.com <63860DbnZ!>
2017-07-16 18:51:31 +0300

    refactor

--- src/main/java/com/test/PhoneBook/controller/UserController.java
+++ src/main/java/com/test/PhoneBook/controller/UserController.java
@@ -1,21 +1,14 @@
 package com.test.PhoneBook.controller;
 
-
-import com.test.PhoneBook.model.UserInfo;
 import com.test.PhoneBook.service.ContactService;
 import com.test.PhoneBook.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-
-import javax.validation.Valid;
 
 @Controller
 @RequestMapping("app")
@@ -33,18 +26,12 @@ public class UserController {
         return "index";
     }
 
-    @GetMapping("login")
-    public ModelAndView login() {
-        ModelAndView mav = new ModelAndView();
-        mav.setViewName("login");
-        return mav;
-    }
-
     @GetMapping("secure/contact-details")
     public ModelAndView getAllUserContacts() {
         ModelAndView mav = new ModelAndView();
         mav.addObject("userContacts", contactService.getAllContacts());
         mav.setViewName("contacts");
+        logger.info("All users shown");
         return mav;
     }
 
@@ -54,39 +41,8 @@ public class UserController {
         String errorMessage = "You are not authorized for the requested data.";
         mav.addObject("errorMsg", errorMessage);
         mav.setViewName("403");
+        logger.info("Error");
         return mav;
     }
-
-    @GetMapping("create-user")
-    public ModelAndView createUserView() {
-        ModelAndView mav = new ModelAndView();
-        mav.setViewName("user-creation");
-        mav.addObject("user", new UserInfo());
-        return mav;
-    }
-
-    @PostMapping("create-user")
-    public ModelAndView createUser(@Valid UserInfo user, BindingResult result) {
-        ModelAndView mav = new ModelAndView();
-        if (result.hasErrors()) {
-            logger.info("Validation errors while submitting form.");
-            mav.setViewName("user-creation");
-            mav.addObject("user", user);
-            return mav;
-        }
-
-        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-        String hashedPassword = passwordEncoder.encode(user.getPassword());
-        UserInfo registration = new UserInfo();
-        registration.setUserName(user.getUserName());
-        registration.setPassword(hashedPassword);
-        registration.setRole("ROLE_USER");
-        userService.create(registration);
-        mav.setViewName("login");
-        //logIn(user);
-        logger.info("User created successfully.");
-        return mav;
-    }
-
 
 } 
