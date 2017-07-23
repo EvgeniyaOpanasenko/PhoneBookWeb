4e75a7fa8beefac6a522bd2dbc5d3ff33d718f03
evgeniya.opanasenko@outlook.com <63860DbnZ!>
2017-07-23 18:30:07 +0300

    TODO add

--- src/main/java/com/test/PhoneBook/controller/ContactController.java
+++ src/main/java/com/test/PhoneBook/controller/ContactController.java
@@ -38,25 +38,47 @@ public class ContactController {
 
 
     @PostMapping(value = "/edit")
-    public ModelAndView editContact(@RequestParam(name = "contactId") Long id, @Valid @ModelAttribute("contact") Contact contact, BindingResult result)
+    public ModelAndView editContact(@RequestParam(name = "contactId") Long id,
+                                    @Valid @ModelAttribute("contact") Contact contact,
+                                    BindingResult result)
             throws SuchContactsExistAllredyException {
 
         ModelAndView mav = new ModelAndView();
         if (result.hasErrors()) {
             logger.info("Validation errors while submitting form");
+            mav.addObject("userContacts", contactService.findOne(id));
             mav.setViewName("contact-details-edit");
+            contact.setId(id);
             mav.addObject("contact", contact);
             return mav;
         }
 
         logger.info("Id to edit Post method " + id);
 
-       /* Contact editableContact = contactService.findOne(id);
-        Contact update = contactService.compareContatcs(editableContact, contact);*/
+        Contact editableContact = contactService.findOne(id);
 
+        //Contact update = contactService.compareContatcs(editableContact, contact);
+
+        //TODO if filed di empty = compare contact with the same in database(by id)
+       /* if (contact.getFirstName().isEmpty()) {
+            contact.setFirstName(editableContact.getFirstName());
+        } else if (contact.getPatronymic().isEmpty()) {
+            contact.setPatronymic(editableContact.getPatronymic());
+        } else if (contact.getLastName().isEmpty()) {
+            contact.setLastName(editableContact.getLastName());
+        } else if (contact.getMail().isEmpty()) {
+            contact.setMail(editableContact.getMail());
+        } else if (contact.getCellPhone().isEmpty()) {
+            contact.setCellPhone(editableContact.getCellPhone());
+        } else if (contact.getHomePhone().isEmpty()) {
+            contact.setHomePhone(editableContact.getHomePhone());
+        } else if (contact.getAddress().isEmpty()) {
+            contact.setAddress(editableContact.getAddress());
+        }*/
+
+       //TODO as an option to set id to editableContact.id
         if (contactService.addContact(contact)) {
             contactService.deleteContact(id);
-            //contact.setId(id);
         }
 
         logger.info("Contact edited ID " + id);
