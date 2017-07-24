60ee6a9820905c9ae79bf830dde0a8ec0eff8585
evgeniya.opanasenko@outlook.com <63860DbnZ!>
2017-07-24 15:15:47 +0300

    file storage add h2 database

--- src/main/java/com/test/PhoneBook/PhoneBookApplication.java
+++ src/main/java/com/test/PhoneBook/PhoneBookApplication.java
@@ -2,11 +2,20 @@ package com.test.PhoneBook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication
+//@ActiveProfiles("test")
+//@Value()
+@PropertySources({
+		@PropertySource(value = "application-embeded.properties")
+})
 public class PhoneBookApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PhoneBookApplication.class, args);
+
+		ConfigurableApplicationContext context = SpringApplication.run(PhoneBookApplication.class, args);
 	}
 }
